b128a184fc5267741ffdbcd56cdf885c7b2cd37f
Werner <werner@LV50>
2023-02-21 12:27:49 +0100

    Consider comm. failure, streamlined example

--- examples/kernel/Failsafe/SlaveApplicationClass.h
+++ examples/kernel/Failsafe/SlaveApplicationClass.h
@@ -20,35 +20,30 @@ class SlaveApplicationClass : public SlaveRtuKernelClass {
 	private:
 	// How many Coils to support
 	static const size_t	_numCoils = 4;
-	// How many Holding Register
-	static const uint8_t _numHoldingRegs = 1;
+
 	
 	// Config params for the application
 	struct ApplicationEeprom : public KernelEeprom {
 		
-		uint16_t holdingValues[_numHoldingRegs];
+		// No deficated application config, but we want to read the
+		// Kernel config settings
 	};
 	
 	ApplicationEeprom _config;
-	void _saveEeprom(void);
+
+
 	
 	// Instance methods connected to kernel callback
-	uint8_t cbAccessHoldingRegisters(bool write, uint16_t address, uint16_t length) override;
 	uint8_t cbAccessCoils(bool write, uint16_t address, uint16_t length) override;
 	
 	void cbDriveFailsafeCoils(bool phase, uint16_t	mask, uint16_t	safeState) override;
-
-	// A set of holding registers 
-	// Register #0 is used for Coils Power-On state !
-	
-	uint16_t 		_holdingRegs[_numHoldingRegs];
-	bool			_updateReceived = false;
 	
 	// A set of Coils, #0 connected to LED
 	// Others may be set here, 0 == not connected
 	uint8_t			_coilPins[_numCoils] = { COIL_LED_PIN, 0, 0, 0};
 	bool 			_coilStates[_numCoils];
 	
+	// Buffer the mask of failsafe coils
 	uint16_t		_failsafeCoils = 0;
 };
 
